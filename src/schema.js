72642e9ebbf28f2923ea2a503f2e4c80f49b5b48
Sebastian Österberg <sebastian.osterberg@gmail.com>
2020-04-05 10:53:09 +0200

    Break out schema

---
+++ src/schema.js
@@ -0,0 +1,41 @@
+module.exports = `
+  type Slot {
+    place: Place
+    uuid: String
+    start: Int
+    end: Int
+  }
+  
+  type Location {
+    lat: Float
+    lng: Float
+  }
+  
+  type Place {
+    place_id: String
+    name: String
+    location: Location
+    photo: String
+    icon: String
+    rating: Float
+    address: String
+    phone_number: String
+    categories: [String]
+    heatmap: [Int]
+  }
+  
+  type Query {
+    nearby(location: String, radius: Int, rankby: String, category: String): [Place]
+    places(name: String, locationbias: String, fields: String): [Place]
+    place(place_id: String): Place
+    favorites(uuid: String): [Place]
+    latest(uuid: String): [Place]
+    next(uuid: String): [Slot]
+  }
+  
+  type Mutation {
+    setFavorite(uuid: String, place_id: String): Boolean
+    removeFavorite(uuid: String, place_id: String): Boolean
+    setSlot(uuid: String, place_id: String, slotStart: Int, slotEnd: Int): Boolean
+  }
+`;
